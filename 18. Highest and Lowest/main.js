76941ecc783dcad3f28ea8105e1ab0f39c250dfc
Vovan1608 <vovan1608@gmail.com>
2021-03-10 09:14:04 +0200

    remove log

--- 18. Highest and Lowest/main.js
+++ 18. Highest and Lowest/main.js
@@ -10,13 +10,12 @@ highAndLow("1 9 3 4 -5"); // return "9 -5"
 
 function highAndLow(numbers) {
   const num = numbers.split(" ");
-  console.log(num);
   const max = Math.max(...num);
   const min = Math.min(...num);
   return `${max} ${min}`;
 }
 
-const res = highAndLow("1 2 3 4 5");
+const res = highAndLow("1 9 3 4 -5");
 console.log(res);
 const add = document.querySelector(".add");
 const div = document.createElement("div");
