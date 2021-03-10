508139fb39919b3e50e4e5c03e9526f05475bd27
Vovan1608 <vovan1608@gmail.com>
2021-03-10 09:03:55 +0200

    added function highAndLow(numbers)

--- 18. Highest and Lowest/main.js
+++ 18. Highest and Lowest/main.js
@@ -1,4 +1,4 @@
-/* 
+/*
 In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 Example:
@@ -7,3 +7,9 @@ highAndLow("1 2 3 4 5");  // return "5 1"
 highAndLow("1 2 -3 4 5"); // return "5 -3"
 highAndLow("1 9 3 4 -5"); // return "9 -5"
 */
+
+function highAndLow(numbers) {
+  const max = Math.max(...numbers);
+  const min = Math.min(...numbers);
+  return `${max} ${min}`;
+}
