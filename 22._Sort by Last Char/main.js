568ba61c6035b6a368059e0ef692276946e6b4c9
Vovan1608 <vovan1608@gmail.com>
2021-03-16 17:56:22 +0200

    added function

--- 22._Sort by Last Char/main.js
+++ 22._Sort by Last Char/main.js
@@ -1,4 +1,19 @@
 /* 
 Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
 If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+x = 'man i need a taxi up to ubud';// ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
 */
+
+function last(x) {
+  return x;
+}
+
+const x = "man i need a taxi up to ubud";
+const res = last(x);
+
+const add = document.querySelector(".add");
+const elem = document.createElement("div");
+elem.innerText = res;
+elem.style = "background: #61dafb";
+
+add.appendChild(elem);
