3f0dede44fb5ff89e5e24bec40c24c02fae05f8e
Vovan1608 <vovan1608@gmail.com>
2021-03-23 20:00:27 +0200

    done 24-th task

--- 24.Even or Odd - Which is Greater/main.js
+++ 24.Even or Odd - Which is Greater/main.js
@@ -5,17 +5,33 @@
 */
 
 function evenOrOdd(str) {
-  let diff = [...str].map(el => Number(el)).reduce((a, b) => b % 2 ? a - b : a + b, 0);
+  let diff = [...str]
+    .map((el) => Number(el))
+    .reduce((a, b) => (b % 2 ? a - b : a + b), 0);
 
   if (diff < 0) {
     return "Odd is greater than Even";
   }
 
-	if (diff > 0) {
+  if (diff > 0) {
     return "Even is greater than Odd";
   }
 
   return "Even and Odd are the same";
 }
 
-const str = "12"; // -> 
+const str = "12"; // -> 'Even is greater than Odd'
+const res = evenOrOdd(str);
+
+const add = document.querySelector("#add");
+add.style = `
+  background-color: #61dafb;
+  font-size: 36px;
+  text-align: center;
+  border: 1px solid black;
+`;
+
+const el = document.createElement("div");
+el.innerText = res;
+
+add.append(el);
