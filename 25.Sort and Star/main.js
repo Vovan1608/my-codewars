66454fb99668a0e8de898194d159304b8c15af02
Vovan1608 <vovan1608@gmail.com>
2021-03-24 10:26:26 +0200

    added sort, join

---
+++ 25.Sort and Star/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* 
+You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+The returned value must be a string, and have "***" between each of its letters.
+You should not remove or add elements from/to the array.
+*/
+
+function twoSort(s) {
+  // const firstEl = ;
+  return [...[...s].sort()[0]].join("***");
+}
+
+const s = [
+  "bitcoin",
+  "take",
+  "over",
+  "the",
+  "world",
+  "maybe",
+  "who",
+  "knows",
+  "perhaps",
+]; // -> 'b***i***t***c***o***i***n'
+
+const add = document.querySelector("#add");
+add.style = `
+	background-color: #61dafb;
+	text-align: center;
+	border: 1px solid #000;
+	font-size: 36px;
+`;
+
+const el = document.createElement("div");
+el.innerText = twoSort(s);
+
+add.append(el);
